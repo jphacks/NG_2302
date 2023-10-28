04eab2d91c62b91457118da53f147ae1a981a33e
ito <88521121+HarumaIto@users.noreply.github.com>
2023-10-28 19:41:32 +0900

    fix: localでHTTP通信できるように変更

--- frontend/src/routes/SignUp.jsx
+++ frontend/src/routes/SignUp.jsx
@@ -15,7 +15,11 @@ export const SignUp = () => {
         };
 
         try {
-            await axios.post('http://localhost:8000/account', signupData);
+            await axios.post('http://127.0.0.1:8000/auth/account', signupData)
+                .then(res => {
+                    console.log(res);
+                    console.log(res.data);
+                });
             setMessage('Account created successfully!');
         } catch (error) {
             console.error('Account creation failed:', error);
