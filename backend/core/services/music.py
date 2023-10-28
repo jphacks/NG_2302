55dc8875ed19fae307b62526e498c183b9b7f488
Akito Shitara <danpany@AkitonoMacBook-Pro.local>
2023-10-29 02:45:49 +0900

    現在再生されている楽曲の情報を取得するAPIを実装

--- backend/core/services/music.py
+++ backend/core/services/music.py
@@ -1,3 +1,4 @@
+from typing import List
 from dataclasses import dataclass
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -6,7 +7,7 @@ from sqlalchemy.orm import Session
 
 from core.constants import SpotifyOAuthConstant, ErrorCode
 from core.daos.spotify import SpotifyApiIdDao
-from core.dtos.music import EnqueueReturnValue
+from core.dtos.music import EnqueueReturnValue, GetMusicInfoReturnValue
 
 
 @dataclass(frozen=True)
@@ -15,25 +16,31 @@ class MusicService:
     spotify_api_id_dao: SpotifyApiIdDao
     account_id: int
 
-    def enqueue(
-        self,
-        music_title: str
-    ) -> EnqueueReturnValue:
-        # Spotifyのインスタンスを取得
+    def _get_spotify_instance(self, scope: List[str]) -> spotipy.Spotify | None:
         spotify_api_id = self.spotify_api_id_dao.read_record_by_account_id(
             db=self.db,
             account_id=self.account_id
         )
         if spotify_api_id is None:
-            return EnqueueReturnValue(error_codes=(ErrorCode.SPOTIFY_NOT_REGISTERED,))
+            return None
 
         # Spotipyのセットアップ
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
             client_id=spotify_api_id.spotify_client_id,
             client_secret=spotify_api_id.spotify_client_secret,
             redirect_uri=SpotifyOAuthConstant.SPOTIPY_REDIRECT_URI,
-            scope=["user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing"]
+            scope=scope
         ))
+        return sp
+
+    def enqueue(
+        self,
+        music_title: str
+    ) -> EnqueueReturnValue:
+        scope = ["user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing"]
+        sp = self._get_spotify_instance(scope)
+        if sp is None:
+            return EnqueueReturnValue(error_codes=(ErrorCode.SPOTIFY_NOT_REGISTERED,))
 
         # Spotifyで曲を検索
         result = sp.search(q=music_title, limit=1)
@@ -51,3 +58,21 @@ class MusicService:
             print(f"Could not find music '{music_title}'")
             return EnqueueReturnValue(error_codes=(ErrorCode.MUSIC_NOT_FOUND,))
         return EnqueueReturnValue(error_codes=())
+
+    def get_music_info(self) -> GetMusicInfoReturnValue:
+        scope = ["user-read-playback-state", "user-read-currently-playing"]
+        sp = self._get_spotify_instance(scope)
+        if sp is None:
+            return GetMusicInfoReturnValue(error_codes=(ErrorCode.SPOTIFY_NOT_REGISTERED,))
+
+        current_playback = sp.current_playback()
+        if current_playback is not None and 'item' in current_playback:
+            music_title = current_playback['item']['name']
+            album_image_url = current_playback['item']['album']['images'][0]['url']
+
+            return GetMusicInfoReturnValue(
+                error_codes=(),
+                title=music_title,
+                image_url=album_image_url
+            )
+        raise Exception("get_music_info failed")
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from routers import (
@@ -9,6 +10,18 @@ from routers import (
 
 # FastAPIの用意
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 
 app.include_router(
     auth.router,
