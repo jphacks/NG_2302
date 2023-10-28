081b7d66d078186dc772ddfc07a0fba7d86f0ea6
ito <88521121+HarumaIto@users.noreply.github.com>
2023-10-29 00:00:39 +0900

    fix: 認証回りの実装

--- frontend/src/routes/SignUp.jsx
+++ frontend/src/routes/SignUp.jsx
@@ -1,30 +1,50 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { useCookies } from 'react-cookie';
 import { Box, Typography, TextField, Button, Link } from '@mui/material';
 import { customTextField } from '../styles/CustomTextField';
+import { useNavigate } from 'react-router-dom';
 
 export const SignUp = () => {
     const [message, setMessage] = useState('');
+    const [cookies, setCookie] = useCookies(['access_token', 'refresh_token', 'token_type']);
+    const navigate = useNavigate();
+
+    const header = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+	}
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        const signupData = {
+        const singUp = {
             login_id: data.get('id'),
             login_password: data.get('password'),
         };
+        const signIn = {
+			username: data.get('id'),
+			password: data.get('password'),
+		};
 
         try {
-            await axios.post('http://127.0.0.1:8000/auth/account', signupData)
+            await axios.post('http://127.0.0.1:8000/auth/account', signUp)
                 .then(res => {
                     console.log(res);
                     console.log(res.data);
                 });
             setMessage('Account created successfully!');
+			const result = await axios.post('http://127.0.0.1:8000/auth/token', signIn, { headers: header });
+            if (result != null) {
+                setCookie('access_token', result.access_token);
+                setCookie('refresh_token', result.refresh_token);
+                setMessage('Login successful!');
+                navigate('/home');
+            }
         } catch (error) {
             console.error('Account creation failed:', error);
-            setMessage('Account creation failed. Please try again.');
         }
+        setMessage('Account creation failed. Please try again.');
+
     };
 
     return (
