25faa203362678b1d48ef96cddb7e4a7f6db0fc7
ito <88521121+HarumaIto@users.noreply.github.com>
2023-11-12 16:28:55 +0900

    fix: 曲情報を取得するリクエストを処理する場所の変更

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState, createContext } from 'react';
-import axios from 'axios';
-import { useCookies } from 'react-cookie';
+import React, { useState, createContext } from 'react';
 import { Routes, Route, Outlet } from "react-router-dom";
 import { Container, Box, AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
@@ -8,16 +6,14 @@ import HomeIcon from '@mui/icons-material/Home';
 import { Home } from "./routes/Home";
 import { SignIn } from './routes/SignIn';
 import { SignUp } from './routes/SignUp';
-import { backendUrl } from './config/backendUrl';
 import { Setting } from './routes/Setting';
 import { QrAuth } from './routes/QrAuth';
-import { withAuthHeader } from './config/Headers';
 import { ModeSelect } from './routes/ModeSelect';
 import { SearchMusic } from './routes/SearchMusic';
 
 export const ModeContext = createContext();
 
-const Layout = ({ children }) => {
+const Layout = () => {
     return (
         <Box
             sx={{
@@ -35,56 +31,7 @@ const Layout = ({ children }) => {
 
 export default function App() {
     const [mode, setMode] = useState('');
-    const [cookies] = useCookies(['access_token']);
-    const [musicInfo, setMusicInfo] = useState({});
     const [trackList, setTrackList] = useState([]);
-    const [elapsedTime, setElapsedTime] = React.useState(0); //経過時間を格納するためのState
-    const updateTime = 3; // updateTimeを変更することで、Timerの更新頻度を変更できる
-    let duration = -1;
-
-    // バックエンドから曲のリストを取得する
-    const getMusicInfo = async () => {
-        try {
-            await axios.get(`${backendUrl}/music/get_queue_info`, withAuthHeader(cookies.access_token))
-                .then((res) => {
-                    duration = res.data.current_music_duration;
-                    setMusicInfo(res.data);
-                });
-            console.log(musicInfo);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    // 初期化時に実行
-    useEffect(() => {
-        const timer = setInterval(() => {
-            // 10秒間隔で更新する
-            setElapsedTime(prevTime => {
-                // durationを超えたら再実行
-                // 曲の残り時間を超えたところで、リロードする
-                if (duration !== -1 && prevTime >= duration) {
-                    // musicInfoをリセットする
-                    getMusicInfo();
-                    // 経過時間リセット
-                    return 0;
-                }
-                // updateTime秒インクリメント
-                return prevTime + updateTime;
-            });
-        }, updateTime * 1000); // updateTime秒 * 1000 = msごと
-
-        return () => clearInterval(timer);
-    }, []);
-
-    // トークンが登録されたら、曲のリストを取得する
-    useEffect(() => {
-        if (cookies.access_token !== undefined) {
-            getMusicInfo();
-        }
-    }, [cookies.access_token]);
-
-
 
     return (
         <Container component="main" maxWidth="xs">
@@ -126,7 +73,7 @@ export default function App() {
                             <Route path="/" element={<ModeSelect />} />
                             <Route path="/signIn" element={<SignIn />} />
                             <Route path="/signUp" element={<SignUp />} />
-                            <Route path="/home" element={<Home musicInfo={musicInfo} setTrackList={setTrackList} />} />
+                            <Route path="/home" element={<Home setTrackList={setTrackList} />} />
                             <Route path="/setting" element={<Setting />} />
                             <Route path="/qrAuth" element={<QrAuth />} />
                             <Route path="/search_music" element={<SearchMusic trackList={trackList} />} />
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { backendUrl } from '../config/backendUrl';
+import { withAuthHeader } from '../config/Headers';
 import { useCookies } from 'react-cookie';
-import { Box } from '@mui/material';
 import { EnqueueTextField } from '../components/EnqueueTextField';
 import { PlayingSong } from '../components/PlayingSong';
 import { SongWaitList } from '../components/SongWaitList';
@@ -13,18 +15,62 @@ import { ModeContext } from '../App';
 import { RegisterModalDialog } from '../components/RegisterModalDialog';
 import { TitleSearchTextField } from '../components/TitleSearchTextField';
 
-export const Home = ({ musicInfo, setTrackList }) => {
+export const Home = ({ setTrackList }) => {
     const [open, setOpen] = useState(false);
+    const [musicInfo, setMusicInfo] = useState({});
     const { mode } = useContext(ModeContext);
     const [cookies] = useCookies(['access_token']);
+    const [elapsedTime, setElapsedTime] = useState(0); //経過時間を格納するためのState
+    const updateTime = 3; // updateTimeを変更することで、Timerの更新頻度を変更できる
+    let duration = -1;
 
+    // バックエンドから曲のリストを取得する
+    const getMusicInfo = async () => {
+        try {
+            await axios.get(`${backendUrl}/music/get_queue_info`, withAuthHeader(cookies.access_token))
+                .then((res) => {
+                    duration = res.data.current_music_duration;
+                    setMusicInfo(res.data);
+                });
+            console.log(musicInfo);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // 初期化時に実行
     useEffect(() => {
         if (mode === ModeTypes.DJ && cookies.access_token === undefined) {
             setOpen(true);
         } else {
             setOpen(false);
         }
+
+        const timer = setInterval(() => {
+            // 10秒間隔で更新する
+            setElapsedTime(prevTime => {
+                // durationを超えたら再実行
+                // 曲の残り時間を超えたところで、リロードする
+                if (duration !== -1 && prevTime >= duration) {
+                    // musicInfoをリセットする
+                    getMusicInfo();
+                    // 経過時間リセット
+                    return 0;
+                }
+                // updateTime秒インクリメント
+                return prevTime + updateTime;
+            });
+        }, updateTime * 1000); // updateTime秒 * 1000 = msごと
+
+        return () => clearInterval(timer);
     }, []);
+
+    // トークンが登録されたら、曲のリストを取得する
+    useEffect(() => {
+        if (cookies.access_token !== undefined) {
+            getMusicInfo();
+        }
+    }, [cookies.access_token]);
 
     return (
         <>
