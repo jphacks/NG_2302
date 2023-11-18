c7b58e763d0cf3e6c078bfbb5e5466f9da44076d
ito <88521121+HarumaIto@users.noreply.github.com>
2023-11-18 13:55:53 +0900

    fix: id & password センター揃え

--- frontend/src/routes/Setting.jsx
+++ frontend/src/routes/Setting.jsx
@@ -34,15 +34,15 @@ export const Setting = () => {
 
             <PageTitle title={'アカウント情報'} />
 
-            <Box sx={{ mt: 1 }} >
+            <Box sx={{ mt: 1 }} textAlign={'center'} >
                 <Typography component="h4" className='text'>
-                    ID
+                    ユーザーID
                 </Typography>
                 <Typography component="h2" variant="div">
                     {cookies.id}
                 </Typography>
             </Box>
-            <Box sx={{ mt: 1, mb: 3 }} >
+            <Box sx={{ mt: 1, mb: 3}} textAlign={'center'} >
                 <Typography component="h4" className='text'>
                     パスワード
                 </Typography>
