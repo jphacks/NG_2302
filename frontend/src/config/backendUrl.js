66e8b40beeae95f9fb0e9c8b7e88ff1008a0de1a
ito <88521121+HarumaIto@users.noreply.github.com>
2024-03-01 22:59:31 +0900

    fix

--- frontend/src/config/backendUrl.js
+++ frontend/src/config/backendUrl.js
@@ -1,4 +1,4 @@
 const dev = 'http://127.0.0.1:8000';
-const prd = 'https://web-mgtirivoza-an.a.run.app/';
+const prd = 'https://web-mgtirivoza-an.a.run.app';
 
-export const backendUrl = prd;
+export const backendUrl = dev;
